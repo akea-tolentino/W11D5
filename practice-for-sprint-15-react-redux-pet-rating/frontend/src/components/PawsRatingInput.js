df988fe0448561cc47f32708f2778732b3883cb2
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-06-09 14:16:09 -0700

    add hover effect

--- practice-for-sprint-15-react-redux-pet-rating/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating/frontend/src/components/PawsRatingInput.js
@@ -1,12 +1,40 @@
+import {useState} from "react";
+
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
 
+const [activeRating, setActiveRating] = useState(rating);
+
   return (
-    <input
-      type="number"
-      disabled={disabled}
-      value={rating}
-      onChange={onChange}
-    />
+    <>
+      <input
+        type="number"
+        disabled={disabled}
+        value={rating}
+        onChange={onChange}
+        />
+      <div className="rating-input">
+        <div onMouseEnter={()=> setActiveRating(1)}
+          className={activeRating > 0 ? "filled" : "empty"} >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div onMouseEnter={() => setActiveRating(2)}
+          className={activeRating > 1 ? "filled" : "empty"} >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div onMouseEnter={() => setActiveRating(3)}
+          className={activeRating > 2 ? "filled" : "empty"} >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div onMouseEnter={() => setActiveRating(4)} 
+          className={activeRating > 3 ? "filled" : "empty"} >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div onMouseEnter={() =>setActiveRating(5)}
+          className={activeRating > 4 ? "filled" : "empty"} >
+          <i className="fa fa-paw"></i>
+        </div>
+      </div>
+    </>
   );
 };
 
