bfb7b3db00ecbf314d53807b03ea529d591987e3
Matthew Montejo <montejododger@yahoo.com>
2023-06-09 14:54:37 -0700

    input disabled

--- practice-for-sprint-15-react-redux-pet-rating/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating/frontend/src/components/PawsRatingInput.js
@@ -1,41 +1,75 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
+    const [activeRating, setActiveRating] = useState(rating);
 
-const [activeRating, setActiveRating] = useState(rating);
-
-  return (
-    <>
-      <input
-        type="number"
-        disabled={disabled}
-        value={rating}
-        onChange={onChange}
-        />
-      <div className="rating-input">
-        <div onMouseEnter={()=> setActiveRating(1)}
-          className={activeRating > 0 ? "filled" : "empty"} >
-          <i className="fa fa-paw"></i>
-        </div>
-        <div onMouseEnter={() => setActiveRating(2)}
-          className={activeRating > 1 ? "filled" : "empty"} >
-          <i className="fa fa-paw"></i>
-        </div>
-        <div onMouseEnter={() => setActiveRating(3)}
-          className={activeRating > 2 ? "filled" : "empty"} >
-          <i className="fa fa-paw"></i>
-        </div>
-        <div onMouseEnter={() => setActiveRating(4)} 
-          className={activeRating > 3 ? "filled" : "empty"} >
-          <i className="fa fa-paw"></i>
-        </div>
-        <div onMouseEnter={() =>setActiveRating(5)}
-          className={activeRating > 4 ? "filled" : "empty"} >
-          <i className="fa fa-paw"></i>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <input
+                type="number"
+                disabled={disabled}
+                value={rating}
+                onChange={onChange}
+            />
+            <div className="rating-input">
+                <div
+                    onMouseEnter={() => {
+                        if (!disabled) setActiveRating(1);
+                    }}
+                    onMouseLeave={() => {
+                        if (!disabled) setActiveRating(rating);
+                    }}
+                    className={activeRating > 0 ? "filled" : "empty"}
+                >
+                    <i className="fa fa-paw"></i>
+                </div>
+                <div
+                    onMouseEnter={() => {
+                        if (!disabled) setActiveRating(2);
+                    }}
+                    onMouseLeave={() => {
+                        if (!disabled) setActiveRating(rating);
+                    }}
+                    className={activeRating > 1 ? "filled" : "empty"}
+                >
+                    <i className="fa fa-paw"></i>
+                </div>
+                <div
+                    onMouseEnter={() => {
+                        if (!disabled) setActiveRating(3);
+                    }}
+                    onMouseLeave={() => {
+                        if (!disabled) setActiveRating(rating);
+                    }}
+                    className={activeRating > 2 ? "filled" : "empty"}
+                >
+                    <i className="fa fa-paw"></i>
+                </div>
+                <div
+                    onMouseEnter={() => {
+                        if (!disabled) setActiveRating(4);
+                    }}
+                    onMouseLeave={() => {
+                        if (!disabled) setActiveRating(rating);
+                    }}
+                    className={activeRating > 3 ? "filled" : "empty"}
+                >
+                    <i className="fa fa-paw"></i>
+                </div>
+                <div
+                    onMouseEnter={() => {
+                        if (!disabled) setActiveRating(5);
+                    }}
+                    onMouseLeave={() => {
+                        if (!disabled) setActiveRating(rating);
+                    }}
+                    className={activeRating > 4 ? "filled" : "empty"}
+                >
+                    <i className="fa fa-paw"></i>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default PawsRatingInput;
